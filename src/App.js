e821278c31a679bcecb71a28c16642aa4ad8d2ec
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-15 15:29:21 +0200

    adds route to homepage and renames .js files for .jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
@@ -7,6 +7,7 @@ import AnonRoute from './components/AnonRoute';
 import Private from './pages/Profile';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import HomePage from './pages/HomePage';
 import { withAuth } from './providers/AuthProvider';
 
 class App extends Component {
@@ -16,15 +17,16 @@ class App extends Component {
 			return <div>loading ... </div>;
 		}
 		return (
-			<div className="">
+			<>
 				<h1>On2Wheels</h1>
 				<Navbar />
 				<Switch>
+					<Route path="/" component={HomePage} />
 					<AnonRoute path="/signup" component={Signup} />
 					<AnonRoute path="/login" component={Login} />
 					<PrivateRoute path="/private" component={Private} />
 				</Switch>
-			</div>
+			</>
 		);
 	}
 }
