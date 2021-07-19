7dd1f69cf619bde79c5ad2e8641585e0cf55c8b9
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-19 19:16:47 +0200

    re-styles the container, cards and content component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Container extends Component {
   render() {
     return (
-      <div className="py-4">
+      <div className="mx-2">
         <h2 className="mb-4">{this.props.title}</h2>
         <div className="flex overflow-x-scroll pb-10 no-scrollbar">
           {this.props.children}
