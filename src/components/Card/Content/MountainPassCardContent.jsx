8fbc83858821650fdb66c7051b04fae766b7a499
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-19 10:43:06 +0200

    adjust styles of card content

--- src/components/Card/Content/MountainPassCardContent.jsx
+++ src/components/Card/Content/MountainPassCardContent.jsx
@@ -7,12 +7,12 @@ export class MountainPassCardContent extends Component {
           <div className="md:flex-shrink-0">
             <img className=" w-full object-cover" src="https://images.unsplash.com/photo-1561390368-a315cfe6833b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" styles={{"height": "320px", "width": "420px"}} alt="default image" />
           </div>
-          <div className="flex justify-between items-start p-4">
+          <div className="flex justify-between items-end p-4">
            <div className="h-1/5">
               <p className="text-lg font-bold">Name</p>
               <p className="subtitle">Province</p>
             </div>
-            <div className="wrapper">
+            <div className="card-content-details">
               <p className="subtitle">Altitud</p>
               <p className="subtitle">Desnivel</p>
             </div>
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,7 +5,7 @@ export class Container extends Component {
     return (
       <div className="py-4">
         <h2 className="mb-4">{this.props.title}</h2>
-        <div className="flex w-full justify-around space-x-2">
+        <div className="flex w-full justify-around space-x-2 ">
           {this.props.children}
         </div>
       </div>
