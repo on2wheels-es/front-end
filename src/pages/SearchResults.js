038d0c6e230058283cb83915bf7158dca22c3fb6
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-22 20:05:00 +0200

    refactors SearchResults

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -10,22 +10,23 @@ import Container from '../components/Container';
 import PrintMunicipalityCard from '../components/Card/PrintMunicipalityCard';
 
 export default function SearchResults(props) {
-  const { search } = useLocation();
-  const queryValues = queryString.parse(search);
-  const { arrival, departure } = queryValues;
+  const [ municipalities, setMunicipalities ] = useState([]);
 
+  const { search } = useLocation();
+  const { arrival, departure } = queryString.parse(search);
+
+  /*
   const calculateMiddleDate = () => {
     const date1 = new Date (arrival)
     const date2 = new Date (departure)
     const middleDate = new Date (date2 - (date2-date1)/2);
     return middleDate;
   }
+  */
 
   // call the Weather API and pass it the middleName
   // eslint-disable-next-line no-unused-vars
-  const middleDate = calculateMiddleDate();
-
-  const [ municipalities, setMunicipalities ] = useState([]);
+  // const middleDate = calculateMiddleDate();
   
   useEffect(() => {
     let mounted = true;
