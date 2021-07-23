40d77e0db3515974927889be60e6f91a56de6a9c
Laia Lloret <laialloret95@gmail.com>
2021-07-23 16:35:55 +0200

    get center point of coordinates and update lat long of the map

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 import queryString from 'query-string';
 import apiClient from '../services/apiClient';
 
-import Map from "../components/Map"
+// import Map from "../components/Map"
 import Header from '../components/Header';
 import Container from '../components/Container';
 import PrintMunicipalityCard from '../components/Card/PrintMunicipalityCard';
@@ -21,15 +21,19 @@ export default function SearchResults(props) {
     return middleDate;
   }
 
+  console.log('location',props.location.search)
+
   // call the Weather API and pass it the middleName
   // eslint-disable-next-line no-unused-vars
   const middleDate = calculateMiddleDate();
 
   const [ municipalities, setMunicipalities ] = useState([]);
+  console.log('municipalities', municipalities)
   
   useEffect(() => {
     let mounted = true;
-    apiClient.getSearchResults(props.location.search)
+    apiClient
+      .getSearchResults(props.location.search)
       .then(response => {
         if(mounted) {
           setMunicipalities(response)
@@ -45,8 +49,8 @@ export default function SearchResults(props) {
       <Header />
       <main>
         <h1 className="mb-5">Search Results</h1>
-        <Map data={municipalities}></Map>
-        < Container>
+        {/* <Map data={municipalities}></Map> */}
+        <Container>
           <PrintMunicipalityCard data={municipalities} />
         </Container>
       </main>
