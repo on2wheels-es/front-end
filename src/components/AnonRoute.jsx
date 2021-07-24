a52282977103c4f47a5fa31486fbc1e65a853b65
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-24 13:29:04 +0200

    restructures signup and login flow

--- src/components/AnonRoute.jsx
+++ src/components/AnonRoute.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { withAuth } from '../providers/AuthProvider';
 
-function AnonRoute({ component: Component, isLoggedIn, ...rest }) {
-	return <Route {...rest} render={props => (!isLoggedIn ? <Component {...props} /> : <Redirect to="/profile" />)} />;
+function AnonRoute({ component: Component, isLoggedIn, redirect, ...rest }) {
+	return <Route {...rest} render={props => (!isLoggedIn ? <Component {...props} /> : <Redirect to={redirect} />)} />;
 }
 
 export default withAuth(AnonRoute);
