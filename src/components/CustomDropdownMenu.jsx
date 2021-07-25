b52e009104055b1dd5aa7b688a4695d0940ffac8
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-25 17:54:07 +0200

    styles react select and makes it multipotion

--- src/components/CustomDropdownMenu.jsx
+++ src/components/CustomDropdownMenu.jsx
@@ -2,37 +2,38 @@ import React, { useState } from "react";
 import Select, { components } from "react-select";
 
 const CustomDropdownMenu = ({data, handleSelectedValue, defaultSelectedValue, placeholder}) => {
-  const [selectedValue, setSelectedValue ] = useState(defaultSelectedValue || '');
+  const [selectedValue, setSelectedValue ] = useState(defaultSelectedValue || null);
+  console.log(selectedValue)
 
   const handleChange = (newSelectedValue) => {
     setSelectedValue(newSelectedValue)
     handleSelectedValue(newSelectedValue)
   }
  
-  const Option = (props) => { 
-    console.log(props.value === selectedValue)
+  const Option  = ({ children, ...props }) => { 
     return ( 
     <div> 
       <components.Option {...props}> 
-        <input type="checkbox" checked={props.isSelected} onChange={() => null}/> <label>{props.label}</label> 
+        <input type="checkbox" checked={props.isSelected} onChange={() => null} /> <label className="mx-1">{props.label}</label> 
       </components.Option> 
     </div> 
     ); 
   };
 
-  const customStyles = {
-    control: (base, state) => ({
-      ...base,
-      border: 0,
-      padding:0,
-      backgroundColor: '#313030',
+   const colourStyles = {
+    control: styles => ({ ...styles, backgroundColor: '#313030', border: 0 }),
+    option: (base) => ({ 
+      ...base, 
+      padding: 5, 
+      borderRadius: 5, 
+      color: '#313030', 
+      display: 'flex' ,
+      alignItems: 'center',
       fontFamily: 'Open sans',
       fontSize: '16px',
-      // Removes weird border around container
-      boxShadow: state.isFocused ? null : null,
-
-    })
-  };
+   }) 
+   }
+  
 
   return (
     <div className="761md:w-72">
@@ -42,7 +43,8 @@ const CustomDropdownMenu = ({data, handleSelectedValue, defaultSelectedValue, pl
         options={data}
         components={{ Option }}
         placeholder={placeholder}
-        styles={customStyles}
+        styles={colourStyles}
+        isMulti
       />
     </div>
   );
