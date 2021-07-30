e7fe52aaeff1b3fd769e3f4674ec6c0df9506d08
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-30 19:17:44 +0200

    starts styling profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -52,86 +52,81 @@ class Profile extends Component {
 				<Header />
 				<main>
 					{ popUpOpen && <NewUserModal onPopUpClose={this.handleClosePopUp}/>}
-					<div>
-						<div>
-							<img src="https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar.png" alt="profile picture" />
-							<div>
-								<ul>
-									<li>{favouriteRoutes.length || 0} Rutas guardadas</li>
-									<li>{favouritePasses.length || 0} Puertos de montañas guardados</li>
-									<li>{favouriteLocations.length || 0} Muncipios guardados</li>
-								</ul>
-							</div>
-						</div>
-						<div>
-              { view ?
-               (
-                 <div>  
-                  <label>Nombre</label>
-                  <input 
-                    type="text" 
-                    name="firstName" 
-                    value={firstName || 'Tu nombre'}
-                    placeholder={firstName || 'Tu nombre'}
-                    readOnly
-                  />
+          { view ?
+              (
+                <div className="flex">
+                    <div className="w-2/5" >
+                      <img src="https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar.png" alt="profile picture" />
+                      <div>
+                        <ul>
+                          <li>{favouriteRoutes.length || 0} Rutas guardadas</li>
+                          <li>{favouritePasses.length || 0} Puertos de montañas guardados</li>
+                          <li>{favouriteLocations.length || 0} Muncipios guardados</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="flex flex-col w-3/5">
+                        <label>Nombre</label>
+                        <input 
+                          type="text" 
+                          name="firstName" 
+                          value={firstName || 'Tu nombre'}
+                          placeholder={firstName || 'Tu nombre'}
+                          readOnly
+                        />
 
-                  <label>Apellidos</label>
-                  <input 
-                    type="text" 
-                    name="lastName" 
-                    value={lastName || 'Tu apellido'}
-                    placeholder={lastName || 'Tu apellido'}
-                    readOnly
-                  />
+                        <label>Apellidos</label>
+                        <input 
+                          type="text" 
+                          name="lastName" 
+                          value={lastName || 'Tu apellido'}
+                          placeholder={lastName || 'Tu apellido'}
+                          readOnly
+                        />
 
-                  <label>Email</label>
-                  <input 
-                    type="text" 
-                    name="email" 
-                    value={email || 'Tu email'}
-                    placeholder={email || 'Tu email'} 
-                    readOnly
-                  />
+                        <label>Email</label>
+                        <input 
+                          type="text" 
+                          name="email" 
+                          value={email || 'Tu email'}
+                          placeholder={email || 'Tu email'} 
+                          readOnly
+                        />
 
-                  <label>Provincia</label>
-                  <input 
-                    type="text" 
-                    name="municipality" 
-                    value={municipality || 'Provincia donde vives'} 
-                    placeholder={municipality || 'Provincia donde vives'} 
-                    readOnly
-                  />
-                  <label>Fecha de nacimiento</label>
-                  <input 
-                    type="text" 
-                    name="birthday" 
-                    value={birthday || 'Fecha de nacimiento'} 
-                    placeholder={birthday || 'Fecha de nacimiento'} 
-                    readOnly
-                  />
+                        <label>Provincia</label>
+                        <input 
+                          type="text" 
+                          name="municipality" 
+                          value={municipality || 'Provincia donde vives'} 
+                          placeholder={municipality || 'Provincia donde vives'} 
+                          readOnly
+                        />
+                        <label>Fecha de nacimiento</label>
+                        <input 
+                          type="text" 
+                          name="birthday" 
+                          value={birthday || 'Fecha de nacimiento'} 
+                          placeholder={birthday || 'Fecha de nacimiento'} 
+                          readOnly
+                        />
 
-                  <label>Género</label>
-                  <input 
-                    type="text" 
-                    name="gender" 
-                    value={gender || 'Género'} 
-                    placeholder={gender || 'Género'} 
-                    readOnly
-                  />
+                        <label>Género</label>
+                        <input 
+                          type="text" 
+                          name="gender" 
+                          value={gender || 'Género'} 
+                          placeholder={gender || 'Género'} 
+                          readOnly
+                        />
 
 
-                  <button onClick={this.changeToEditMode}>Editar información</button>
+                        <button onClick={this.changeToEditMode}>Editar información</button>
                   </div>
-               )
-              
+                </div>
+              )
               :
-               (
-                 < EditProfileForm />
-               ) 
-              }
-						</div>
-					</div>
+               (< EditProfileForm />) 
+            }
 				</main>
 			</>
 		);
