baf5d35f88f9bf2eeeb463e3bb0bcf6ee30be6ed
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-30 18:37:40 +0200

    styles signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,10 +31,9 @@ class Login extends Component {
     return (
       <div className="flex w-screen h-screen bg-secundary-medium">
         <div className="hidden md:w-1/2 md:block">
-        <path xmlns="http://www.w3.org/2000/svg" d="M143.492,221.863L336.226,29.129c6.663-6.664,6.663-17.468,0-24.132c-6.665-6.662-17.468-6.662-24.132,0l-204.8,204.8    c-6.662,6.664-6.662,17.468,0,24.132l204.8,204.8c6.78,6.548,17.584,6.36,24.132-0.42c6.387-6.614,6.387-17.099,0-23.712    L143.492,221.863z"/>
           <img className="object-cover object-bottom w-full h-full" src={loginImage}/>
         </div>
-        <div className="wrapper px-8 md:px-36 py-8 text-white md:w-1/2 md:h-screen bg-secundary-medium">
+        <div className="wrapper px-8 py-8 text-white bg-secundary-medium md:px-36 md:w-1/2 md:h-screen xl:py-48">
             <Link to="/"> 
                <p className="mb-8 hover:underline">{`< Volver a la página`}</p>
             </Link>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import 'react-notifications/lib/notifications.css';
 import { Link } from "react-router-dom";
 import { withAuth } from "../providers/AuthProvider";
+import signinImage from "../images/signin-image.jpg"
 
 
 class Signup extends Component {
   constructor(props) {
     super(props)
     this.state = {
-		email: "",
-		password: ""
-	};
+    email: "",
+    password: ""
+  };
   }
 
   handleFormSubmit = event => {
@@ -27,33 +28,57 @@ class Signup extends Component {
   render() {
     const { email, password } = this.state;
     return (
-			<div className="form-container">
-				<div className="text-center">
-					<Link to="/">on2Wheels Logo</Link>
-					<h2 className="text-center font-extrabold text-gray-900">Register</h2>
-				</div>
-				<form className="form" onSubmit={this.handleFormSubmit}>
-					<div className="rounded-md shadow-sm flex-col space-y-2">
-						<label className="block text-lg font-medium text-gray-700">Email:</label>
-						<input type="email" name="email" value={email} onChange={this.handleChange} className="form-input" />
-						<label className="block text-lg font-medium text-gray-700">Password</label>
-						<input
-							type="password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-							className="form-input"
-						/>
-					</div>
-					<input className="button-indigo mt-4" type="submit" value="Signup" />
-				</form>
-				<p className="font-xl font-bold mt-4 text-indigo-700">
-					Already have account?
-					<Link to={'/login'}> Login</Link>
-				</p>
-			</div>
-		);
+      <div className="flex w-screen h-screen bg-secundary-medium">
+        <div className="hidden md:w-1/2 md:block">
+          <img className="object-cover object-center w-full h-full" src={signinImage}/>
+        </div>
+        <div className="wrapper px-8 py-8 text-white bg-secundary-medium md:px-36 md:w-1/2 md:h-screen xl:py-48">
+            <Link to="/"> 
+               <p className="mb-8 hover:underline">{`< Volver a la página`}</p>
+            </Link>
+            <div className="mx-auto">
+              <div className="py-8 border-b mb-8">
+                <h2 className="mb-4">Crea una cuenta nueva</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Non ac imperdiet ullamcorper lacus. Duis.</p>
+              </div>
+              <form className="flex flex-col space-y-8" onSubmit={this.handleFormSubmit}>
+                <div>
+                  <label className="caption_regular_light">Email</label>
+                  <input 
+                    type="email" 
+                    name="email" 
+                    value={email} 
+                    onChange={this.handleChange} 
+                    className="form-input" 
+                  />
+                </div>
+                  <div>
+                    <label className="caption_regular_light">Contraseña</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={this.handleChange}
+                      className="form-input"
+                    />
+                  </div>
+                  <input className="button-accent-big" type="submit" value="Crear cuenta" />
+              </form>
+              <div className="flex my-6 items-center justify-between">
+                  <hr className="w-5/12"></hr>
+                  <p>O</p>
+                  <hr className="w-5/12"></hr>
+              </div>
+              <Link to="/signup">
+                <p className="text-center font-normal hover:font-heavy hover:text-primary-medium hover:underline focus:underline focus:text-primary-medium">Ya tienes una cuenta? Accede a ella!</p>
+              </Link>
+            </div>
+          </div>
+      </div>
+    );
   }
 }
+
 
 export default withAuth(Signup);
