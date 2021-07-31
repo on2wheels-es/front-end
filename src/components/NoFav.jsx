8c683d2afacf25731414abeb407c834683d87f62
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-31 11:39:49 +0200

    styles favourite page

--- src/components/NoFav.jsx
+++ src/components/NoFav.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
-import noResults from '../images/no-results.jpeg';
 import { Link } from 'react-router-dom';
+import bikeNoFav from '../images/bikeNotFav.png'
 
 class NoFav extends Component {
 
     render() {
         return (
-          <>
-            <img src={noResults} alt="broken-bike" />
-            <p>Aún no tienes {this.props.type}</p>
-            <button className="button-accent-searchBar"> 
-                <Link to="/">Explora</Link>
-            </button>
-          </>
+          <div className="flex flex-col items-center space-y-4 md:flex-row">
+            <div className="w-28">
+              <img src={bikeNoFav} alt="Not favourites yet" />
+            </div>
+            <div className="flex flex-col space-y-4 text-center md:text-left md:ml-12 ">
+              <h3>Aún no has añadido {this.props.type}</h3>
+              <p>Haz clic en el corazón para añadir a favoritos.</p>
+              <button className="button-accent w-1/3 self-center md:self-start"> 
+                  <Link to="/">Explora</Link>
+              </button>
+            </div>
+          </div>
         )
     }
 }
